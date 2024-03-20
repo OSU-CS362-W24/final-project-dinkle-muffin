5084c83fa217bf7c32d98e72f585e9859029e31c
Sean McCoy <mccoyse@oregonstate.edu>
2024-03-20 12:18:22 -0700

    integration test to verify that inputs are unaltered on pressing add button

--- src/chartBuilder/chartBuilder.test.js
+++ src/chartBuilder/chartBuilder.test.js
@@ -60,3 +60,29 @@ test("7 pairs of input fields are on page when add value button is clicked 6 tim
     expect(domTesting.queryAllByLabelText(document, "X")).toHaveLength(7)
     expect(domTesting.queryAllByLabelText(document, "Y")).toHaveLength(7)
 })
+
+test("Clicking add value button does not effect inputs of already existing input fields", async function() {
+    // Arrange
+    initDomFromFiles(CHART_BUILDER_PAGE_HTML_PATH, CHART_BUILDER_PAGE_JS_PATH)
+
+    const addValueButton = domTesting.getByText(document, "+")
+    let xInputs = domTesting.queryAllByLabelText(document, "X")
+    let yInputs = domTesting.queryAllByLabelText(document, "Y")
+
+    // Act
+    const user = userEvent.setup()
+    await user.type(xInputs[0], "0")
+    await user.type(yInputs[0], "10")
+    await user.click(addValueButton)
+    xInputs = domTesting.queryAllByLabelText(document, "X")
+    yInputs = domTesting.queryAllByLabelText(document, "Y")
+    await user.type(xInputs[1], "1")
+    await user.type(yInputs[1], "20")
+    await user.click(addValueButton)
+
+    // Assert
+    expect(xInputs[0]).toHaveValue(0)
+    expect(yInputs[0]).toHaveValue(10)
+    expect(xInputs[1]).toHaveValue(1)
+    expect(yInputs[1]).toHaveValue(20)
+})
